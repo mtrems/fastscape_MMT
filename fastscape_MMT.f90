2bc53e24b7d7ec8ca40b6f7e98db41965f248c7b
Marissa Tremblay <mtrems@gmail.com>
2016-03-07 19:58:08 -0800

    Complete stream power portion of model
    Added the following:
    
    1) Fixed issue associated with assignment of boundary conditions.
    
    2) Added a recursive subroutine to build the stack as described in
    Braun and Willett (2013).
    
    2) Added “color” tracker of which nodes belong to which basins.
    
    3) Use the inverse stack to calculate upstream drainage area at each
    node.
    
    4) Evolve the landscape using the discretized form of the stream power
    equation.
    
    5) Write landscape info at certain time steps to file. Also write the
    long profile for the longest river network at the end of the model to
    file.
    
    6) Add time stepping.

--- fastscape_MMT.f90
+++ fastscape_MMT.f90
@@ -2,14 +2,17 @@ program fastscape_MMT
 
 implicit none !this is good fortran practice
 
-integer i,j,ij,ii,jj,ij_max_slope,ijk
+integer i,j,ij,ii,jj,ij_max_slope,ijk,istep,nfreq,nprint
 integer nstep,nx,ny,nn
+integer nstack,ncolor
 double precision xl,yl,dt,dx,dy,totaltime,k,m,n,u,drop,max_drop,length_max_slope
-double precision slope,max_slope,length_drop
+double precision slope,max_slope,length_drop,c
 
-integer,dimension(:),allocatable:: rec,fixed,ndon
+integer,dimension(:),allocatable:: rec,ndon,stack,color,stack2id,longestriver
 integer,dimension(:,:),allocatable:: donors
-double precision,dimension(:),allocatable:: h,length
+double precision,dimension(:),allocatable:: h,x,y,length,area
+
+character cs*4
 
 nstep = 1000 !number of time steps
 nx = 101 !number of nodes in the x-direction
@@ -27,31 +30,50 @@ dy = yl/(ny-1) !grid size in y-direction, in meters
 k = 1.e-4 !erodibility/susceptibility constant, in units of...?
 n = 1. !slope exponent, a constant
 m = 0.4 !area exponent, a constant
-u = 2.e-3 !uplift rate, in units of...?
+u = 2.e-3 !uplift rate, in units of meters per year
 
-allocate (h(nn),rec(nn),length(nn),fixed(nn),ndon(nn))
+nfreq = 20 !frequency with which to output results to a text file
+nprint = 0 !counter for printing
+
+allocate (h(nn),x(nn),y(nn),rec(nn),length(nn),ndon(nn))
 allocate (donors(8,nn))
+allocate (stack(nn),color(nn),stack2id(nn))
+allocate (area(nn))
 
-call random_number(h) !fortran random number generator will not return negative values
+!initialize variables
+do i = 1,nn
+   h(i) = 0
+   x(i) = 0
+   y(i) = 0
+enddo
 
-!here, give each node in the model which is not on the edge of the model space an elevation using the random number generator 
-do j=2,ny-1
-   do i=2,nx-1
-      ij=i+(j-1)*nx !we are indexing this using only one number, ij. 
-      h(ij)=0.+h(ij)
+!define x and y positions
+do j = 1,ny
+   do i = 1,nx
+      ij = i+(j-1)*nx
+      x(ij) = dx*i - dx
+      y(ij) = dy*j - dy
    enddo
 enddo
-!print*,'height',h !print the height to test if working
+
+!here, give each node in the model which is not on the edge of the model space an elevation using the random number generator 
+do j = 2,ny-1
+   do i = 2,nx-1
+      ij = i+(j-1)*nx !we are indexing this using only one number, ij. 
+      call random_number(h(ij)) 
+      !fortran random number generator will not return negative values
+   enddo
+enddo
+
+do istep = 1,nstep
 
 !initialize rec and length arrays
-!fixed=1 !initialize fixed. we haven't done anything with this yet
 do ij = 1,nn
    rec(ij) = ij 
 enddo 
 length=0
 
 !Calculate flow directions and build receiver array
-print*,"Calculating flow directions and building receiver array"
 do j = 2,ny-1
    do i = 2,nx-1
       ij = i+(j-1)*nx 
@@ -61,8 +83,10 @@ do j = 2,ny-1
       length_max_slope=0 
       do jj=j-1,j+1
          do ii=i-1,i+1 !look at the eight nodes adjacent to ij
-            drop=h(ij)-h(ii+(jj-1)*nx) !calculate the change in elevation with all adjacent nodes
-            length_drop=((dx*(ii-i))**2 + (dy*(jj-j))**2)**0.5 !calculate the distance between all adjacent nodes
+            !calculate the change in elevation with all adjacent nodes
+            drop=h(ij)-h(ii+(jj-1)*nx) 
+            !calculate the distance between all adjacent nodes
+            length_drop=((dx*(ii-i))**2 + (dy*(jj-j))**2)**0.5             
             slope=drop/length_drop !calculate the slope with all adjacent nodes
             if (slope.gt.max_slope)then 
 !if the slope between node ij and an adjacent node is greater than the previously defined maximum slope, change slope, index, and length
@@ -72,23 +96,124 @@ do j = 2,ny-1
             endif
          enddo
       enddo
-      rec(ij) = ij_max_slope !loop through all adjacent nodes, define the receiver as the node with the maximum slope
+      !loop through adjacent nodes, define the receiver as the node with the maximum slope
+      rec(ij) = ij_max_slope 
       length(ij) = length_max_slope !define the length corresponding to the receiver array
    enddo
 enddo
-print*,"Receivers calculated!"
 
 !initialize ndon and donors arrays
 ndon=0 
 donors=0
 
 !Calculate donor array and number of donors for each cell
-print*,"Calculating donor array"
-do ij = 1,nn 
-   ijk = rec(ij)
-   ndon(ijk) = ndon(ijk) + 1 !adds 1 to ndon, the number of donors, for each receiver of a given donor
-   donors(ndon(ijk),ijk) = ij !say that for node ijk, one of my donors is ij
+do ij = 1,nn
+   if (rec(ij).ne.ij) then 
+      ijk = rec(ij)
+      !adds 1 to ndon, the number of donors, for each receiver of a given donor
+      ndon(ijk) = ndon(ijk) + 1 
+      donors(ndon(ijk),ijk) = ij !say that for node ijk, one of my donors is ij
+   endif
 enddo
-print*,"Donors calculated!"
 
-end
+!Build the stack, a list of all nodes in an order that makes computing discharge easy
+nstack = 0
+ncolor = 0
+
+do ij = 1,nn
+   stack(ij) = 0
+   color(ij) = 0
+   stack2id(ij) = 0 
+enddo
+
+do ij = 1,nn
+   if(rec(ij).eq.ij) then
+     nstack = nstack + 1
+     ncolor = ij
+     stack(nstack) = ij
+     color(nstack) = ncolor
+     stack2id(ij) = nstack
+     !call recursive function
+     call add_to_stack (ij,nstack,stack,ndon,donors,nn,ncolor,color,stack2id)
+   endif
+enddo
+
+!set drainage area of each node to the size of a single node initially
+do ij = 1,nn
+   area(ij) = dx*dy
+enddo
+
+!calculate upstream drainage area using the reverse stack
+do ij = nn,1,-1
+   ijk = stack(ij)
+   if (rec(ijk).ne.ijk) then
+      area(rec(ijk)) = area(rec(ijk)) + area(ijk)
+   endif
+enddo
+
+!evolve the landscape
+do j = 2,ny-1
+   do i = 2,nx-1
+      ijk = i+(j-1)*nx
+      !do the uplift component to first. Do not apply to nodes on the boundary.
+      h(ijk) = h(ijk) + (u*dt) 
+      enddo
+enddo
+!then do the erosion component. Only apply this to nodes who are not their own receivers (i.e., not boundaries and not sinks)
+do i = 1,nn
+   ijk = stack(i)
+   if (rec(ijk).ne.ijk) then
+         c = k*(area(ijk)**m)*dt*(1./length(ijk))
+         h(ijk) = (h(ijk) + (c*h(rec(ijk))))*(1./(1.+c))
+   endif
+enddo
+
+!for writing results to file. Gives each time step saved a unique file name
+write(cs,'(i4)') istep
+if (istep.lt.10) cs(1:3)='000'
+if (istep.lt.100) cs(1:2)='00'
+if (istep.lt.1000) cs(1:1)='0'
+
+!write x, y, height, color ID, drainage area, and time in the model to file
+if (mod(istep,nfreq).eq.0) then
+   open(unit=8,status="new",file="out"//cs//".txt")
+   do i = 1,nn
+      write(8,*) x(i),y(i),h(i),color(i),area(i),istep*dt
+   enddo
+endif
+close(8)
+
+enddo
+
+!extract the longest river profile from the landscape
+open (7,file='profile.txt',status='unknown')
+i = maxloc(area,1)
+do while (ndon(i).ne.0)
+   i = donors(maxloc(area(donors(1:ndon(i),i)),1),i)
+   write(7,*) length(i),h(i),area(i),(h(i)-h(rec(i)))/length(i)
+enddo
+close(7)
+
+end program fastscape_MMT
+
+!Recursive subroutine that will go to progressively upstream nodes and build the stack
+recursive subroutine add_to_stack (ij,nstack,stack,ndon,donors,nn,ncolor,color,stack2id)
+
+implicit none
+
+integer nn,stack(nn),ndon(nn),donors(8,nn),color(nn),stack2id(nn)
+integer ij,j,nstack,ncolor
+
+
+
+do j = 1,ndon(ij)
+      nstack = nstack + 1
+      stack(nstack) = donors(j,ij)
+      color(nstack) = ncolor
+      stack2id(ij) = nstack
+      call add_to_stack(donors(j,ij),nstack,stack,ndon,donors,nn,ncolor,color,stack2id)
+enddo
+
+return
+
+end subroutine add_to_stack
